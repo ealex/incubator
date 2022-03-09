75d9a6494c638a82ce1a80faaedcdf3f098bb035
Ene Alexandru <ene.alexandru@gmail.com>
2022-03-09 13:20:27 +0200

    version that can keep temperature

--- arduino/incubator/incubator.ino
+++ arduino/incubator/incubator.ino
@@ -1,5 +1,6 @@
-// general libraries
+ // general libraries
 #include <SPI.h>
+
 
 //display
 #include <Adafruit_GFX.h>    // Core graphics library
@@ -8,6 +9,9 @@
 #define TFT_RST        8 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         9
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+#define UI_TASK_TIMER (100) // run ui task each 100ms
+void uiDisplayCallback();
+
 
 //DHT22
 #include "DHT.h"
@@ -17,17 +21,77 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 #define DHT_2_PIN   5
 DHT dht_heater(DHT_1_PIN, DHT_TYPE);
 DHT dht_env(DHT_2_PIN, DHT_TYPE);
-float heater_humidity;
-float heater_temperature;
-float env_humidity;
-float env_temperature;
+volatile float heater_humidity;
+volatile float heater_temperature;
+volatile float env_humidity;
+volatile float env_temperature;
+volatile bool heater_loop_heartbeat = false;
+#define SENSOR_TASK_TIMER (1000)
+void sensorReadCallback();
+
+
+// temperature control
+#define HEATER_1  (A0)
+#define HEATER_2  (A1)
+#define TEMP_LOW_HISTEREZIS  ((float)0.0)
+#define TEMP_HIGH_HISTEREZIS  ((float)0.1)
+#define TEMP_COLD_LIMIT       ((float)37.0)
+#define TEMP_WARM_LIMIT       ((float)38.0)
+#define HEATER_MAX_TEMP  ((float)50)
+#define LOW_TEMP_ALARM_LIMIT ((float)
+volatile float tempSetPoint=37.5;
+#define CONTROL_TASK_TIMER  (500)  //it's a slow process, so no hurry
+void tempControlCallback();
+
+
+// motor control
+#define MOTOR_1   (A2)
+volatile uint16_t motorTickInterval = 60;
+volatile uint16_t motorTickDuration = 2;
+#define MOTOR_TASK_TIMER  (1000)  // control motor in slow intervals
+void motorControlCallback();
+
+
+// interface encoder button
+// https://github.com/Stutchbury/EncoderButton
+#include <EncoderButton.h>
+#define ENCODER_A   (A2)
+#define ENCODER_B   (A3)
+#define ENCODER_BUT (A4)
+EncoderButton uiButton(ENCODER_A, ENCODER_B, ENCODER_BUT);
+void uiControlTask();
+void onUiButtonHandler(EncoderButton& eb);
+volatile boolean uiHeaterEnabled=false;
+volatile boolean uiHeaterBlink=false;
+volatile boolean uiHeaterOverTemp=false;
+volatile boolean uiHeaterUnderTemp=false;
+volatile boolean uiHeaterError = false;
+#define HEATER_DEBUG
+
+
+// task manager
+#include <TaskScheduler.h>
+Scheduler runner;
+Task uiTask(UI_TASK_TIMER, TASK_FOREVER, &uiDisplayCallback, &runner, true);  //user interface handling task
+Task sensorTask(SENSOR_TASK_TIMER, TASK_FOREVER, &sensorReadCallback, &runner, true); // read sensors
+Task controlTask(CONTROL_TASK_TIMER, TASK_FOREVER, &tempControlCallback, &runner, true); // control heaters and alarms
+Task motorTask(MOTOR_TASK_TIMER, TASK_FOREVER, &motorControlCallback, &runner, true); // control egg movement motor
 
 
 
-float p = 3.1415926;
+
+//************************* CODE STARTS HERE *********************
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // configure heater and motor pins
+  pinMode(HEATER_1, OUTPUT);
+  pinMode(HEATER_2, OUTPUT);
+  pinMode(MOTOR_1, OUTPUT);
+  digitalWrite(HEATER_1,LOW);  
+  digitalWrite(HEATER_2,LOW);
+  digitalWrite(MOTOR_1,LOW);
 
   // configure DHT pins and power both sensors
   pinMode(DHT_POWER,OUTPUT);
@@ -35,95 +99,181 @@ void setup() {
   dht_heater.begin();
   dht_env.begin();
 
-  tft.setSPISpeed(1000);
+  // configure encode button
+  uiButton.setEncoderHandler(onUiButtonHandler);
+
+  // start the display
+  // tft.setSPISpeed(1000);
   tft.init(240, 240,SPI_MODE2);
+  tft.setRotation(2);
   tft.fillScreen(ST77XX_BLACK);
+
+  // start task sched.
+  runner.startNow();
 }
 
 
-void tftPrintTest() {
-  tft.setTextWrap(false);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(0, 30);
-  tft.setTextColor(ST77XX_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_GREEN);
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
-  delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST77XX_GREEN);
-  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST77XX_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print(" seconds.");
+
+void onUiButtonHandler(EncoderButton& eb) {
+  
 }
 
 void loop() {
-  // read temperature sensors
-  heater_humidity = dht_heater.readHumidity();
-  heater_temperature = dht_heater.readTemperature();
-  env_humidity = dht_env.readHumidity();
-  if(isnan(env_humidity)) {
-    env_humidity = 99.0;
-  }
-  env_temperature = dht_env.readTemperature();
-  if(isnan(env_temperature)) {
-    env_temperature = 99.0;
-  }
+  // encoder handler
+  uiButton.update();
 
-  // display the data
-  tft.setCursor(0, 0);
-  tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
-  tft.setTextSize(4);
-  tft.print("T:");
-  tft.print(heater_temperature,1);
-  tft.print("C ");
-  tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
-  tft.setTextSize(1);
-  tft.print(env_temperature,2);
-  tft.print("C");
-  tft.setTextSize(4);
-  tft.println("");
-  
-  tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
-  tft.setTextSize(4);
-  tft.print("H:");
-  tft.print(heater_humidity,1);
-  tft.print("% ");
-  tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
-  tft.setTextSize(1);
-  tft.print(env_humidity,1);
-  tft.println("%");
-  
-  delay(2000);
-  
+  // task sched 
+  runner.execute();
 }
 
-void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
+
+//update sensor data
+void sensorReadCallback() {
+  heater_temperature = dht_heater.readTemperature();
+  heater_humidity = dht_heater.readHumidity();
+  env_humidity = dht_env.readHumidity();
+  env_temperature = dht_env.readTemperature();
+}
+
+
+// control heater elements
+void tempControlCallback() {
+  // in case of error freeze everything and start buzzer
+  uiHeaterError = false;
+  heater_loop_heartbeat = (heater_loop_heartbeat)? false:true;
+  if(isnan(env_temperature)) { // unable to work if env. temperature is faulty
+    uiHeaterError=true;
+  } else {
+    if (!isnan(heater_temperature) && (HEATER_MAX_TEMP <= heater_temperature)) { // stop heater if heater_temperature > 50 and if tere's an usable heater temperature sensor
+      digitalWrite(HEATER_1,LOW);
+    } else {
+      // start heater if temp < (TSET-HISTEREZIS)
+      if( env_temperature < (tempSetPoint-TEMP_LOW_HISTEREZIS)) {
+        digitalWrite(HEATER_1,HIGH);
+      }
+      // stop heater if temp > (TSET+HISTEREZIS)
+      if(env_temperature > (tempSetPoint+TEMP_HIGH_HISTEREZIS)) {
+        digitalWrite(HEATER_1,LOW);
+      }
+      
+      uiHeaterOverTemp = (env_temperature > TEMP_WARM_LIMIT);
+      uiHeaterUnderTemp = (env_temperature <TEMP_COLD_LIMIT);
+    }
+  }
+
+  // handle error cases
+  if(true==uiHeaterError) {
+    digitalWrite(HEATER_1,LOW);
+  }
+  uiHeaterEnabled = digitalRead(HEATER_1)?true:false;
+
+  // blink the heater symbol
+  if(uiHeaterEnabled) {
+    uiHeaterBlink = (uiHeaterBlink)?false:true;
+  } else {
+    uiHeaterBlink = false;
+  }
+  
+  
+#ifdef HEATER_DEBUG
+  Serial.print("Temp:");  
+  Serial.print(env_temperature,1);
+  Serial.print(",Heater:");
+  Serial.print(uiHeaterEnabled);
+  Serial.print(",Humidity:");
+  Serial.println(env_humidity,1);
+#endif
+}
+
+void motorControlCallback() {
+  // empty for now
+}
+
+void uiDisplayCallback() {
+  displayMeasuredData();
+}
+
+
+void displayMeasuredData() {
+  uint16_t textColor;
+  uint16_t textBackGround;
+  textBackGround= ST77XX_BLACK;
+  if(uiHeaterError) {
+    textColor = ST77XX_RED;
+  } else {
+    if (uiHeaterOverTemp) {
+      textColor = ST77XX_YELLOW;
+    } else if (uiHeaterUnderTemp) {
+      textColor = ST77XX_BLUE;
+    } else {
+      textColor = ST77XX_GREEN;      
+    }
+  }
+
+  tft.drawLine(0,0,239,0,textBackGround);
+  tft.drawLine(0,1,239,1,textBackGround);
+  tft.drawLine(0,2,239,2,textBackGround);
+  tft.drawLine(0,3,239,3,textBackGround);
+  tft.drawLine(0,4,239,4,textBackGround);
+  tft.drawLine(0,5,239,5,textBackGround);
+  tft.setCursor(20, 5);
+  tft.setTextColor(textColor, textBackGround);
+  tft.setTextSize(6);
+  if(isnan(env_temperature)) {
+    tft.print("--.-");
+  } else {
+    tft.print(env_temperature,1);
+  }
+  tft.print(char(0xF7));
+  tft.println("C");
+  // display humidity 
+  tft.setCursor(20, tft.getCursorY());
+  if(isnan(env_humidity)) {
+    tft.print("--.-");
+  } else {
+    tft.print(env_humidity,1);
+  }
+  tft.print("%");
+  if(uiHeaterBlink) {
+    tft.println(char(0x07));
+  } else {
+    if(heater_loop_heartbeat) {
+      tft.println(char(0x09));
+    } else {
+      tft.println(" ");
+    }
+  }
+  tft.drawLine(0, tft.getCursorY()+10, 239, tft.getCursorY()+10, ST77XX_MAGENTA);
+
+
+  // and now display the set temperature
+  tft.setCursor(10, 120);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.setTextSize(3);
+  tft.print("TEMP. DORITA ");
+  tft.setCursor(0, 150);
+  tft.setTextSize(5);
+  tft.print(" ");
+  tft.print(tempSetPoint,1);
+  tft.print(char(0xF7));
+  tft.println("C ");
+
+  // display some debug data
+  tft.setCursor(0, 220);
+  tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.print("Aux: ");
+  if(isnan(heater_temperature)) {
+    tft.print("--.-");
+  } else {
+    tft.print(heater_temperature,1);
+  }
+  tft.print(char(0xF7));
+  tft.print("C, ");
+  if(isnan(heater_humidity)) {
+    tft.print("--.-");
+  } else {
+    tft.print(heater_humidity,1);
+  }
+  tft.print("%  ");
 }
