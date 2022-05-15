e1795b2b968d6c962335bf4e14f89425296f4c65
Ene Alexandru <ene.alexandru@gmail.com>
2022-05-15 17:28:14 +0300

    port to FreeRTOS multi-core, and the new hardware

--- arduino/incubator_rp2040/incubator_rp2040.ino
+++ arduino/incubator_rp2040/incubator_rp2040.ino
@@ -1,332 +1,325 @@
 // general libraries
 #include <SPI.h>
+#include "pico/stdlib.h"
+
+#include <FreeRTOS.h>
+#include "task.h"
+static const UBaseType_t uxCore0AffinityMask = 0x01;
+static const UBaseType_t uxCore1AffinityMask = 0x02;
 
 
-//display
+/*********** CORE 0 TASKS ****************************/
+// led blink task
+void TaskBlink( void *pvParameters );
+TaskHandle_t xTaskBlinkHandle;
+volatile bool ledStatus;
+
+//display and user interface
 #include <U8g2lib.h>
 U8G2_ST7565_EA_DOGM128_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 17, /* dc=*/ 21, /* reset=*/ 20);
-char displayTextBuffer[64];
+#define TASK_DISPLAY_MAX_DELAY  (TickType_t)(5000)
+void vTaskDisplay( void *pvParameters );
+TaskHandle_t xTaskDisplayHandle;
 
-//DHT22
+// button handling
+#include "Button2.h"
+#define BUTTON_UP_PIN   13
+#define BUTTON_X_PIN    14
+#define BUTTON_DOWN_PIN 15
+Button2 buttonUp, buttonX, buttonDown;
+void click(Button2& btn);
+
+
+
+
+/*********** CORE 1 TASKS ****************************/
+// temperature sensor
 #include "DHT.h"
 #define DHT_TYPE  DHT22
-#define DHT_POWER (0)
-#define DHT_PIN   (1)
+#define DHT_PIN   (27)
 DHT tempSensor(DHT_PIN, DHT_TYPE);
 volatile float heater_humidity;
 volatile float heater_temperature;
-volatile bool heater_loop_heartbeat = false;
-#define SENSOR_TASK_TIMER (1000)
-void sensorReadCallback();
+#define TASK_DHT_DELAY  (TickType_t)(1000)
+void vTaskDHT(void *pvParameters);
+TaskHandle_t xTaskDHTHandle;
+
+// motor control task
+#define MOTOR_PIN   (22)
+#define TASK_MOTOR_DELAY  (TickType_t)(100000) //run the task once per 100s
+volatile uint8_t motorOnTime =0; // how long is the motor on, from 0 to 100 (s) 
+void vTaskMotor(void *pvParameters);
+TaskHandle_t xTaskMotorHandle;
+
+// pid control task
+#include <PID_v1.h>
+#define PID_PIN (26)
+#define TASK_PID_DELAY  (TickType_t)(1000)
+void vTaskPid(void *pvParameters);
+TaskHandle_t xTaskPidHandle;
+double consKp=1, consKi=0.05, consKd=0.25;
+double Setpoint, Input, Output;
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+
+// adc read task
+volatile float vBat;
+#define TASK_ADC_DELAY  (TickType_t)(1000)
+#define ADC_PIN   (28)
+void vTaskADC(void *pvParameters);
+TaskHandle_t xTaskADCHandle;
 
 
-// temperature control
-#define HEATER_1  (3)
-#define TEMP_LOW_HISTEREZIS  ((float)0.0)
-#define TEMP_HIGH_HISTEREZIS  ((float)0.1)
-#define TEMP_COLD_LIMIT       ((float)38)
-#define TEMP_WARM_LIMIT       ((float)45)
-#define HEATER_MAX_TEMP  ((float)50)
-#define LOW_TEMP_ALARM_LIMIT ((float)
-volatile float tempSetPoint = 42;
-#define CONTROL_TASK_TIMER  (1000)  //it's a slow process, so no hurry
-void tempControlCallback();
-
-
-// motor control
-#define MOTOR_1   (4)
-volatile uint16_t motorTickInterval = 60;
-volatile uint16_t motorTickDuration = 2;
-#define MOTOR_TASK_TIMER  (1000)  // control motor in slow intervals
-void motorControlCallback();
-
-
-// interface encoder button
-#define BUTTON_UP     (5)
-#define BUTTON_DOWN   (6)
-#define BUTTON_ENA    (7)
-#define UI_TASK_ENCODER (100)
-void uiEncoderCallback();
-
-
-// user interface globals
-volatile boolean uiHeaterEnabled = false;
-volatile boolean uiHeaterBlink = false;
-volatile boolean uiHeaterOverTemp = false;
-volatile boolean uiHeaterUnderTemp = false;
-volatile boolean uiHeaterError = false;
-#define HEATER_DEBUG
-
-
-boolean ledStatus = true;
 
 //************************* CODE STARTS HERE *********************
-void setup() {
-  // put your setup code here, to run once:
+void setup() {    
+  // setup code for core 0
   Serial.begin();
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("BOOT!!!");
 
-
-  // configure heater and motor pins
-  pinMode(HEATER_1, OUTPUT);
-  pinMode(MOTOR_1, OUTPUT);
-  digitalWrite(HEATER_1, LOW);
-  digitalWrite(MOTOR_1, LOW);
-
-  // configure DHT pins
-  pinMode(DHT_PIN, INPUT);
-  digitalWrite(DHT_PIN, HIGH);
-  tempSensor.begin();
-
-  // configure encode button
-  pinMode(BUTTON_UP, INPUT_PULLUP);
-  pinMode(BUTTON_DOWN, INPUT_PULLUP);
-  pinMode(BUTTON_ENA, INPUT_PULLUP);
-
-  // start the display
-  //SPI.setRX(16);
-  //SPI.setCS(17);
+  // SPI port used by the display
   SPI.setSCK(18);
   SPI.setTX(19);
   SPI.begin(false);
 
+  // get all tasks up and running
+  // status led blink task, core 0, low prio
+  xTaskCreate( TaskBlink, (const portCHAR *)"TaskBlink",  1024,  NULL ,  6,  &xTaskBlinkHandle );
+  vTaskCoreAffinitySet(xTaskBlinkHandle, uxCore0AffinityMask);
+
+  // display task - core 0 low prio
+  xTaskCreate( vTaskDisplay, (const portCHAR *)"vTaskDisplay",  1024,  NULL ,  7,  &xTaskDisplayHandle );
+  vTaskCoreAffinitySet(xTaskDisplayHandle, uxCore0AffinityMask);
+
+
+  // DHT update CORE 1
+  xTaskCreate( vTaskDHT, (const portCHAR *)"vTaskDHT",  1024,  NULL ,  2,  &xTaskDHTHandle );
+  vTaskCoreAffinitySet(xTaskDHTHandle, uxCore1AffinityMask);
+
+  // egg turner control CORE 1
+  xTaskCreate( vTaskMotor, (const portCHAR *)"vTaskMotor",  1024,  NULL ,  7,  &xTaskMotorHandle );
+  vTaskCoreAffinitySet(xTaskMotorHandle, uxCore1AffinityMask);
+
+  // PID controller
+  xTaskCreate( vTaskPid, (const portCHAR *)"vTaskPid",  1024,  NULL ,  2,  &xTaskPidHandle );
+  vTaskCoreAffinitySet(xTaskPidHandle, uxCore1AffinityMask);
+
+  // VBAT adc
+  //xTaskCreate( vTaskADC, (const portCHAR *)"vTaskADC",  1024,  NULL ,  6,  &xTaskADCHandle );
+  //vTaskCoreAffinitySet(xTaskADCHandle, uxCore1AffinityMask);
+
+
+  // start ui buttons
+  buttonUp.begin(BUTTON_UP_PIN);
+  buttonUp.setClickHandler(click);
+
+  buttonX.begin(BUTTON_X_PIN);
+  buttonX.setClickHandler(click);
+
+  buttonDown.begin(BUTTON_DOWN_PIN);
+  buttonDown.setClickHandler(click);
+
+}
+
+
+/***  CORE 0 related code ***/
+// task to control the LED, low priority
+void TaskBlink( void *pvParameters ) {
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN,LOW);
+  
+  while(true) {
+    if(ledStatus) {
+      digitalWrite(LED_BUILTIN,HIGH);
+      ledStatus = false;
+    } else {
+      digitalWrite(LED_BUILTIN,LOW);
+      ledStatus = true;
+    }
+
+    // wait some time
+    // xTaskNotify(xTaskDisplayHandle, 0x00, eSetValueWithOverwrite);
+    vTaskDelayUntil( &xLastExecutionTime, 1000);
+  }
+
+  // should not reach this point
+  vTaskDelete( NULL );
+}
+
+
+/**  
+ *   This task will handle the user interface and display refresh
+ *   the task stays disable untill is woken up by one of the following triggers:
+ *    - DHT22 data read
+ *    - button press (up / down / action)
+ *    - power supply voltage adc read
+ *    - system status monitor (power supply status, 
+ */
+void vTaskDisplay( void *pvParameters ) {
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  char displayTextBuffer[64];
   u8g2.begin();
-  // use a fixed font and a fixed contrast
+  u8g2.setDisplayRotation(U8G2_R2);
   u8g2.setContrast(100);
   u8g2.setFontDirection(0);
-  // start task sched.
+  while(true) {
+    // wait for a notification
+    ulTaskNotifyTake( pdTRUE, portMAX_DELAY);
+
+    //redraw screen
+    u8g2.clearBuffer();
+    u8g2.setFontMode(0);
+    u8g2.setDrawColor(1);
+
+    
+    // draw the current temperature and humidity
+    u8g2.setFont(u8g2_font_lubB14_tf);  
+    sprintf(displayTextBuffer,"%02.1f%cC", heater_temperature, 0xb0);
+    u8g2.drawStr(2,16,displayTextBuffer);  // write something to the internal memory
+    u8g2.drawVLine(64,0,18);
+    sprintf(displayTextBuffer,"%02.1f%%",heater_humidity);
+    u8g2.drawStr(67,16,displayTextBuffer);  // write something to the internal memory
+    u8g2.drawHLine(0,18,128);
+
+    // draw egg turner settings and status
+    u8g2.setFont(u8g2_font_6x12_tf);
+    sprintf(displayTextBuffer, "Motor %03d/100", motorOnTime);
+    u8g2.drawStr(2, 28, displayTextBuffer);
+    u8g2.setFont(u8g2_font_open_iconic_embedded_1x_t);
+    u8g2.drawGlyph(90, 28, 66); // motor ON
+    
+    u8g2.setFont(u8g2_font_6x12_tf);
+    sprintf(displayTextBuffer,"Output:%03.0f", Output);
+    u8g2.drawStr(2, 36, displayTextBuffer);
+
+    u8g2.setFont(u8g2_font_open_iconic_embedded_2x_t);
+    u8g2.drawGlyph(0, 60, 73); // battery full
+    u8g2.drawGlyph(128/2-8, 60, 64); // battery
+    u8g2.drawGlyph(128-16, 60, 71); // warning
+
+    // draw the screen
+    u8g2.sendBuffer();
+  }  
+  vTaskDelete( NULL );
 }
 
 
-
+/**
+ * idle loop
+ *  runs the button loops over here
+ */
 void loop() {
-  // task sched
-  heater_temperature = tempSensor.readTemperature();
-  heater_humidity = tempSensor.readHumidity();
+  // only buttons handling here
+  buttonUp.loop();
+  buttonX.loop();
+  buttonDown.loop();
+}
 
-  u8g2.clearBuffer();
-  u8g2.setFontMode(0);
-  u8g2.setDrawColor(1);
+/**
+ * buttons action callback
+ *  this will set some status flags
+ *  it will notify the display task
+ */
+void click(Button2& btn) {
+  // TODO
+}
+
+
+
+
+/***  CORE 1 related code ***/
+
+/**
+ * updates the humidty and temperature data from the DHT sensor
+ *  - this task runs once every second
+ *  - on read it will notify the following tasks:
+ *      - xTaskDisplay via xTaskDisplayHandle
+ *      - xTaskPID  via xTaskPIDHandle
+ *  - if any of the 2 values are not readable it will set the general alarm flag
+ */
+void vTaskDHT(void *pvParameters) {
+  // configure DHT pins
+  pinMode(DHT_PIN, INPUT);
+  digitalWrite(DHT_PIN, HIGH);
+  tempSensor.begin();
   
-  // draw the current temperature and humidity
-  u8g2.setFont(u8g2_font_lubB14_tf);
-  sprintf(displayTextBuffer,"temp: %02.1f%cC", heater_temperature, 0xb0);
-  u8g2.drawStr(2,16,displayTextBuffer);  // write something to the internal memory
-  sprintf(displayTextBuffer,"umid: %02.1f%%",heater_humidity);
-  u8g2.drawStr(2,32,displayTextBuffer);  // write something to the internal memory
-  u8g2.drawHLine(0,34,128);
-  u8g2.drawHLine(0,35,128);
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  while(true) {
+    heater_temperature = tempSensor.readTemperature();
+    heater_humidity = tempSensor.readHumidity();
+    
+    // notify the display task
+    xTaskNotify(xTaskDisplayHandle, 0x00, eSetValueWithOverwrite);
+    
+    // notify the PID task
+    // TODO
 
-  // graphic symbols to display status
-  u8g2.setFont(u8g2_font_open_iconic_embedded_2x_t);
-  if(ledStatus) {
-    u8g2.drawGlyph(0, 52, 64); // battery
-  } else {
-    u8g2.drawGlyph(0, 52, 67); // plugged in
+    // sleep until next interval
+    vTaskDelayUntil( &xLastExecutionTime, TASK_DHT_DELAY);
   }
-  if(ledStatus) {
-    u8g2.drawGlyph(16, 52, 71); // warning
-  } else {
-    u8g2.drawGlyph(16, 52, 0); // normal
-  }
-  u8g2.drawGlyph(32, 52, 66); // motor ON
-  u8g2.drawGlyph(48, 52, 72); // CONFIG
-  u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
-  u8g2.drawGlyph(64, 52, 69); // heater ON
+  vTaskDelete( NULL );
+}
+
+
+// control the egg turner motor
+void vTaskMotor(void *pvParameters) {
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  uint32_t runTime;
+  uint32_t sleepTile;
+  pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN,LOW);
   
-  // temperature and motor control
-  u8g2.setFont(u8g2_font_6x12_tf);
-  sprintf(displayTextBuffer,"Tset:%02.1f%cC", tempSetPoint, 0xb0);
-  u8g2.drawStr(2, 63, displayTextBuffer);
+  while(true) {
+    runTime = motorOnTime * 1000;
+    sleepTile = TASK_MOTOR_DELAY - runTime;
+    if(0==motorOnTime) {
+      // motor is disabled
+      digitalWrite(MOTOR_PIN,LOW);
+      vTaskDelayUntil( &xLastExecutionTime, TASK_MOTOR_DELAY);
+    } else {
+      // turn the motor on
+      digitalWrite(MOTOR_PIN,HIGH);
+      vTaskDelayUntil( &xLastExecutionTime, runTime);
+      digitalWrite(MOTOR_PIN,LOW);
+      vTaskDelayUntil( &xLastExecutionTime, sleepTile);
+    }
+  }
+  vTaskDelete( NULL );
+}
 
 
-  u8g2.setDrawColor(1);
-  u8g2.setFontMode(1);
-  u8g2.drawBox(66,54,64,13);
-  u8g2.setDrawColor(0);
-  sprintf(displayTextBuffer,"Mset:1/100");
-  u8g2.drawStr(67, 63, displayTextBuffer);
-  u8g2.sendBuffer();
+/**
+ * this task updates the PID loop
+ *  it's output goes straight to the PWM that drives the power transistors
+ *  the PWM frequency is set to 100Hz - to reduce EMI / long wire issues
+ */
+void vTaskPid(void *pvParameters) {
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  Setpoint = 37.5;
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetSampleTime(TASK_PID_DELAY);
+  pinMode(PID_PIN, OUTPUT);
+
+  // starts the PWM output
+  analogWriteFreq(100); // 100Hz
+  analogWriteRange(255);  // from 0 to 255
   
-  Serial.print("Temp:");
-  Serial.println(heater_temperature, 1);
-  Serial.print(",Humidity:");
-  Serial.println(heater_humidity, 1);
-  Serial.println();
-
-  //tempControlCallback();
-  //uiDisplayCurrentTempCallback();
-  if (true == ledStatus) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    ledStatus = false;
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-    ledStatus = true;
+  while(true) {
+    Input = heater_temperature;
+    myPID.Compute();
+    analogWrite( PID_PIN, Output);
+    vTaskDelayUntil( &xLastExecutionTime, TASK_PID_DELAY);
   }
-  delay(1000);
-}
-
-//update sensor data
-void sensorReadCallback() {
-  heater_temperature = tempSensor.readTemperature();
-  heater_humidity = tempSensor.readHumidity();
+  vTaskDelete( NULL );
 }
 
 
-// control heater elements
-void tempControlCallback() {
-  // in case of error freeze everything and start buzzer
-  uiHeaterError = false;
-  heater_loop_heartbeat = (heater_loop_heartbeat) ? false : true;
-  if (isnan(heater_temperature)) { // unable to work if env. temperature is faulty
-    uiHeaterError = true;
-  } else {
-    if (!isnan(heater_temperature) && (HEATER_MAX_TEMP <= heater_temperature)) { // stop heater if heater_temperature > 50 and if tere's an usable heater temperature sensor
-      digitalWrite(HEATER_1, LOW);
-    } else {
-      // start heater if temp < (TSET-HISTEREZIS)
-      if ( heater_temperature < (tempSetPoint - TEMP_LOW_HISTEREZIS)) {
-        digitalWrite(HEATER_1, HIGH);
-      }
-      // stop heater if temp > (TSET+HISTEREZIS)
-      if (heater_temperature > (tempSetPoint + TEMP_HIGH_HISTEREZIS)) {
-        digitalWrite(HEATER_1, LOW);
-      }
-
-      uiHeaterOverTemp = (heater_temperature > (tempSetPoint + TEMP_HIGH_HISTEREZIS));
-      uiHeaterUnderTemp = (heater_temperature < (tempSetPoint - TEMP_LOW_HISTEREZIS));
-    }
-  }
-
-  // handle error cases
-  if (true == uiHeaterError) {
-    digitalWrite(HEATER_1, LOW);
-  }
-  uiHeaterEnabled = digitalRead(HEATER_1) ? true : false;
-
-#ifdef HEATER_DEBUG
-  Serial.print("Temp:");
-  Serial.print(heater_temperature, 1);
-  Serial.print(",Heater:");
-  Serial.print(uiHeaterEnabled);
-  Serial.print(",Humidity:");
-  Serial.println(heater_humidity, 1);
-#endif
-}
-
-void motorControlCallback() {
-  // empty for now
-}
-
-void uiDisplayCallback() {
-  //displayMeasuredData();
-  //displayInterfaceData();
-  //displayDebugData();
-}
-/*
-void uiDisplayCurrentTempCallback() {
-  uint16_t textColor;
-  uint16_t textBackGround;
-  textBackGround = ST77XX_BLACK;
-  if (uiHeaterError) {
-    textColor = ST77XX_RED;
-  } else {
-    if (uiHeaterOverTemp) {
-      textColor = ST77XX_YELLOW;
-    } else if (uiHeaterUnderTemp) {
-      textColor = ST77XX_BLUE;
-    } else {
-      textColor = ST77XX_GREEN;
-    }
-  }
-
-  tft.setCursor(20, 5);
-  tft.setTextColor(textColor, textBackGround);
-  tft.setTextSize(6);
-  if (isnan(heater_temperature)) {
-    tft.print("--.-");
-  } else {
-    tft.print(heater_temperature, 1);
-  }
-  tft.print(char(0xF7));
-  tft.println("C");
-}
-*/
-/*
-void uiDisplayCurrentHumidityCallback() {
-  uint16_t textColor;
-  uint16_t textBackGround;
-  textBackGround = ST77XX_BLACK;
-  if (uiHeaterError) {
-    textColor = ST77XX_RED;
-  } else {
-    if (uiHeaterOverTemp) {
-      textColor = ST77XX_YELLOW;
-    } else if (uiHeaterUnderTemp) {
-      textColor = ST77XX_BLUE;
-    } else {
-      textColor = ST77XX_GREEN;
-    }
-  }
-  // display humidity
-  tft.setCursor(20, 60);
-  if (isnan(heater_humidity)) {
-    tft.print("--.-");
-  } else {
-    tft.print(heater_humidity, 1);
-  }
-  tft.print("%");
-  if (uiHeaterEnabled) {
-    tft.println(char(0x07));
-  } else {
-    if (heater_loop_heartbeat) {
-      tft.println(char(0x09));
-    } else {
-      tft.println(" ");
-    }
+/**
+ * 
+ */
+void vTaskADC(void *pvParameters) {
+  TickType_t xLastExecutionTime = xTaskGetTickCount();
+  while(true) {
+    vBat = analogRead(ADC_PIN);  
+    vTaskDelayUntil( &xLastExecutionTime, TASK_ADC_DELAY);
   }
 }
-*/
-
-/*
-void uiDisplayCurrentSetPointCallback() {
-  // and now display the set temperature
-  tft.setCursor(10, 120);
-  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
-  tft.setTextSize(3);
-  tft.print("TEMP. DORITA ");
-  tft.setCursor(0, 150);
-  tft.setTextSize(5);
-  tft.print(" ");
-  tft.print(tempSetPoint, 1);
-  tft.print(char(0xF7));
-  tft.println("C ");
-}
-*/
-
-/*
-void uiDisplayDebugDataCallback() {
-  // display some debug data
-  tft.setCursor(0, 205);
-  tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
-  tft.setTextSize(1);
-  tft.print("Aux: ");
-  if (isnan(heater_temperature)) {
-    tft.print("--.-");
-  } else {
-    tft.print(heater_temperature, 1);
-  }
-  tft.print(char(0xF7));
-  tft.print("C, ");
-  if (isnan(heater_humidity)) {
-    tft.print("--.-");
-  } else {
-    tft.print(heater_humidity, 1);
-  }
-  tft.print("%  ");
-  tft.println("");
-  tft.println("debug line 2");
-}
-*/
